d95eba8b224265748dda2eacb151bdd0e937f1f4
hyeon81 <guswly801@gmail.com>
2022-03-21 18:36:50 +0900

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import StartPage from "./pages/StartPage";
+
+ReactDOM.render(<StartPage />, document.getElementById("root"));
---
+++ src/pages/StartPage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./StartPage.css";
+
+import { AiOutlineLeft, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { BiCopy } from "react-icons/bi";
+
+export default function StartPage() {
+  return (
+    <>
+    <div className="container">
+      <gnb>
+        <AiOutlineLeft/>
+        <AiOutlineSearch/>
+        <AiOutlineMenu/>
+      </gnb>
+      <div className="title">
+        <h1>조별과제 속 나의 모습은?</h1>
+        <p>총 16개의 유형의 MBTI 성향을 기반으로 <br></br>조별과제 속 나의 모습을 알아보아요.</p>
+      </div>
+
+      <button className="home_button">
+        <p>테스트하기</p>
+        <BsFillArrowRightCircleFill />
+      </button>
+
+      <button className="home_button">
+        <p>링크 복사</p>
+        <BiCopy />
+      </button>
+    </div>
+    </>
+  );
+}
